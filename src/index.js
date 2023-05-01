fd5bce091aa99a0eb1abd8aaa28125c6d3bf6d6a
VadymMakovii <makovii.vadym@gmail.com>
2023-05-01 23:48:17 +0300

    Changed basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tech-boost">
+    <BrowserRouter basename="/goit-boost">
       <App />
     </BrowserRouter>
   </React.StrictMode>
